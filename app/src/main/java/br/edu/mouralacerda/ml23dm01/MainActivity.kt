f0fbb282fc99fad82d6e64b1137841717e681c58
Felipe Ripoli <feliperipoli3@gmail.com>
2023-09-12 21:55:22 -0300

    mudança geral

--- app/src/main/java/br/edu/mouralacerda/ml23dm01/MainActivity.kt
+++ app/src/main/java/br/edu/mouralacerda/ml23dm01/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.mouralacerda.ml23dm01
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -12,16 +13,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botao = findViewById<ImageButton>(R.id.btnProximo1)
+        val nome = findViewById<EditText>(R.id.edtNome)
+        val email = findViewById<EditText>(R.id.edtEmail)
 
         botao.setOnClickListener {
 
-            val nome = findViewById<EditText>(R.id.edtNome).text.toString()
-            val email = findViewById<EditText>(R.id.edtEmail).text.toString()
-
             val pacote = Bundle()
-
-            pacote.putString("nome", nome)
-            pacote.putString("email", email)
+            pacote.putString("nome", nome.text.toString())
+            pacote.putString("email", email.text.toString())
 
             val i = Intent(this, Tela02::class.java)
 
--- app/src/main/java/br/edu/mouralacerda/ml23dm01/Tela02.kt
+++ app/src/main/java/br/edu/mouralacerda/ml23dm01/Tela02.kt
@@ -1,11 +1,34 @@
 package br.edu.mouralacerda.ml23dm01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 
 class Tela02 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela02)
+
+        val nome = intent.extras!!.getString("nome")!!
+        val email =  intent.extras!!.getString("email")!!
+
+        val curso = findViewById<EditText>(R.id.edtCurso)
+        val periodo = findViewById<EditText>(R.id.edtPeriodo)
+        val botao = findViewById<EditText>(R.id.edt)
+
+        botao.setOnClickListener {
+
+            val pacote = Bundle()
+
+            pacote.putString("nome", nome)
+            pacote.putString(("email", email)
+            pacote.putString("curso", curso.text.toString())
+            pacote.putString("curso", periodo.text.toString())
+
+            val i = Intent(this, Tela02::class.java)
+                i.putExtras(pacote)
+        }
     }
 }
